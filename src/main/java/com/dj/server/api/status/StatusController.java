d58425aa7780938f4522a53e82a823cd52f29186
pursue503 <pursue503@naver.com>
2021-07-25 17:55:22 +0900

    + 상태 확인 로그 추가

--- src/main/java/com/dj/server/api/status/StatusController.java
+++ src/main/java/com/dj/server/api/status/StatusController.java
@@ -1,15 +1,18 @@
 package com.dj.server.api.status;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class StatusController {
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
+        log.info("상태: success");
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
