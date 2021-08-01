d23834e55ed079b2ccd7782c8b490f17c439b4d6
chaenabi <find1086@gmail.com>
2021-08-01 21:37:11 +0900

    #8 try to make test for member exception

--- src/test/java/com/dj/server/exception/member/MemberExceptionTest.java
+++ src/test/java/com/dj/server/exception/member/MemberExceptionTest.java
@@ -1,19 +1,29 @@
 package com.dj.server.exception.member;
 
+import com.dj.server.api.member.entity.Member;
+import com.dj.server.api.member.entity.MemberRepository;
+import com.dj.server.api.member.entity.MemberRole;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
 class MemberExceptionTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test(ex)
+
+    @Test
     @Order(1)
     @DisplayName("회원의 데이터를 생성합니다. 첫번째로 실행됩니다.")
     @Transactional(readOnly = true)
@@ -32,14 +42,7 @@ class MemberExceptionTest {
         assertThat(member.getMemberId()).isEqualTo(memberId);
         assertThat(member.getMemberSnsId()).isEqualTo(kakaoId);
         assertThat(member.getMemberNickName()).isEqualTo(nickName);
-        System.out.println("member.getMemberId()" + member.getMemberId());
+
+        assertThat(memberRepository.save(Member.builder().memberSnsId(kakaoId).memberNickName(nickName).memberRole(MemberRole.USER).build())).hasNoNullFieldsOrPropertiesExcept();
     }
-
-    @Test
-    @Order(2)
-    @DisplayName("회원의 닉네임을 변경합니다.")
-    @Transactional
-    public void updateMemberNickName()
-
-
 }
