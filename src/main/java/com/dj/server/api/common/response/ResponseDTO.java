dee00af6ca2b218bc222fdfa301f96c3fc505c79
pursue503 <pursue503@naver.com>
2021-08-04 20:43:25 +0900

    #19 Update ResponseDTO @Param Comment

--- src/main/java/com/dj/server/api/common/response/ResponseDTO.java
+++ src/main/java/com/dj/server/api/common/response/ResponseDTO.java
@@ -6,14 +6,14 @@ import org.springframework.http.HttpStatus;
 
 /**
  *
- * 와일드 카드를 사용하여 여러 타입을 받으면서
+ * 제네릭 파라매터 를 사용하여 여러 타입을 받으면서
  * 어떤 데이터가 와도 Json 으로 변환하여 공통된 클래스로
  * 데이터를 전달 해 주는 클래스
  *
  * @author JaeHyun
  * @created 2021-08-04
  * @since 0.0.1
- * @param <T>
+ * @param <T> --> 주로 Entity 에 대한 ResponseDTO 가 파라매터로 들어오게 됩니다.
  *
  */
 
