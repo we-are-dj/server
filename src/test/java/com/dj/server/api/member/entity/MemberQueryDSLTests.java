0e2eace5e0d9066f20ad89174f6e43305a1ec688
pursue503 <pursue503@naver.com>
2021-08-04 11:40:09 +0900

    #21 Added QueryDSL Test

--- src/test/java/com/dj/server/api/member/entity/MemberQueryDSLTests.java
+++ src/test/java/com/dj/server/api/member/entity/MemberQueryDSLTests.java
@@ -1,6 +1,41 @@
 package com.dj.server.api.member.entity;
 
 
+import com.dj.server.common.dummy.member.MemberDummy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("QueryDsl 을 테스트 합니다")
+@DataJpaTest
 public class MemberQueryDSLTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private final MemberDummy memberDummy = MemberDummy.getInstance();
+
+
+    @Test
+    @Order(1)
+    @DisplayName("QueryDsl 테스트")
+    public void queryDSLTest() {
+
+        //given
+        final String nickName = "홍길동";
+        Member memberId = memberRepository.save(memberDummy.toEntity());
+
+        //when -> QueryDsl
+        Member member = memberRepository.findByMemberNickName(nickName);
+
+
+        assertThat(member.getMemberNickName()).isEqualTo(nickName);
+
+    }
 
 }
