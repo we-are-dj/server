eea79385f7d2323ad9b39914dd951950439c5b6c
chaenabi <find1086@gmail.com>
2021-08-18 15:11:45 +0900

    #46 change response data format as hashmap

--- src/main/java/com/dj/server/api/member/controller/MemberController.java
+++ src/main/java/com/dj/server/api/member/controller/MemberController.java
@@ -16,6 +16,8 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -67,8 +69,10 @@ public class MemberController {
             @ApiResponse(code = 404, message = "회원이 존재하지 않습니다")
     })
     @PatchMapping("/members/nickname")
-    public ResponseDTO<String> updateNickName(@RequestParam("nickname") String nickName) {
-       return new ResponseDTO<>(memberService.updateNickName(nickName), "SUCCESS", HttpStatus.OK);
+    public ResponseDTO<Map<String, String>> updateNickName(@RequestParam("nickname") String nickName) {
+        Map<String, String> map = new HashMap<>();
+        map.put("nickname", memberService.updateNickName(nickName));
+        return new ResponseDTO<>(map, "SUCCESS", HttpStatus.OK);
     }
 
 }
