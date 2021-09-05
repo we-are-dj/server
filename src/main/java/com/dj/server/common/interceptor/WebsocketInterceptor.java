2cc11971fa149643c48b16ea77fe9664c3a33f9a
chaenabi <find1086@gmail.com>
2021-09-05 21:15:10 +0900

    restore WebsocketInterceptor

---
+++ src/main/java/com/dj/server/common/interceptor/WebsocketInterceptor.java
@@ -0,0 +1,43 @@
+package com.dj.server.common.interceptor;
+
+import com.dj.server.common.exception.common.BizException;
+import com.dj.server.common.exception.member.enums.MemberPermitErrorCode;
+import com.dj.server.common.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebsocketInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String accessToken = accessor.getFirstNativeHeader("access_token");
+
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+//            validateWebSocketToken(accessToken);
+        } else if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
+
+        } else if (StompCommand.SEND == accessor.getCommand()) {
+//            validateWebSocketToken(accessToken);
+        }
+        return message;
+    }
+
+    private void validateWebSocketToken(String accessToken) {
+        if (!jwtUtil.isValidAccessToken(accessToken))
+            throw new BizException(MemberPermitErrorCode.ACCESS_TOKEN_EXPIRED);
+    }
+
+
+}
