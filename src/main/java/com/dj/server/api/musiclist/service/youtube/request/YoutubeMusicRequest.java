b07da9caa3914d55d2b506c3c1d272db9434383b
chaenabi <find1086@gmail.com>
2021-09-21 13:44:55 +0900

    #90 fix bug: get expected search result

--- src/main/java/com/dj/server/api/musiclist/service/youtube/request/YoutubeMusicRequest.java
+++ src/main/java/com/dj/server/api/musiclist/service/youtube/request/YoutubeMusicRequest.java
@@ -1,20 +1,20 @@
 package com.dj.server.api.musiclist.service.youtube.request;
 
 import com.dj.server.api.musiclist.model.vo.youtube.YoutubeData;
-import com.dj.server.common.jwt.JwtUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.UriUtils;
+
+import java.net.URI;
 
 /**
  * 실제 Youtube Data api 요청을 위한 클래스입니다.
@@ -34,25 +34,27 @@ public class YoutubeMusicRequest {
         HttpHeaders header = new HttpHeaders();
         header.add("Content-type", MediaType.APPLICATION_JSON + ";charset=UTF-8");
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://www.googleapis.com/youtube/v3/search")
+        UriComponents components = UriComponentsBuilder.fromHttpUrl("https://www.googleapis.com/youtube/v3/search")
                 .queryParam("part", "snippet")
                 .queryParam("key", YTapiKey)
-                .queryParam("order", "relevance")
-                .queryParam("maxResults", "25")
-                .queryParam("q", UriUtils.encode(keyword, "UTF-8"))
-                .queryParam("safeSearch", "strict");
+                .queryParam("maxResults", "5")
+                .queryParam("q", keyword)
+                .queryParam("safeSearch", "strict")
+                .build(false);
+
+        URI uri = components.toUri();
 
         HttpEntity<String> youtubeMusicRequest = new HttpEntity<>(header);
         HttpEntity<String> result = new RestTemplate().exchange(
-                builder.toUriString(),
+                uri,
                 HttpMethod.GET,
                 youtubeMusicRequest,
                 String.class);
+
         return getBody(result);
     }
 
     private YoutubeData getBody(HttpEntity<String> result) throws JsonProcessingException {
-        System.out.println(result.getBody());
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         return mapper.readValue(result.getBody(), YoutubeData.class);
